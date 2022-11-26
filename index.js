d50d9839489282bdbff4515d842b864019af4fce
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-11-26 21:27:41 +0600

    added jwt token

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
+
+const jwt = require('jsonwebtoken');
 
 app.use(cors())
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bkopbwy.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const cardDataCollection = client.db('assignmentTwelve').collection('cardData')
+
+        const usersCollection = client.db('assignmentTwelve').collection('users')
+
+        app.get('/cardData', async (req, res) => {
+            const query = {};
+            const cards = await cardDataCollection.find(query).toArray();
+            res.send(cards);
+        });
+
+        app.get('/cardData/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const card = await cardDataCollection.findOne(query);
+            res.send(card);
+        });
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '7d' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
 
 app.get('/', async (req, res) => {
     res.send('assignment twelve server is running');
